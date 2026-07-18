19d293baf41da8ee891b5b85800d730a092a0e10
Nadeem <veenus.rehman@gmail.com>
2026-07-18 12:01:06 +0500

    Day 4: functions, parameters, NaN edge case

---
+++ function-practice.js
@@ -0,0 +1,11 @@
+/*function greetClient(name) {
+  return "Hello " + name + ", welcome to Armans.AI";
+}
+console.log(greetClient("Ahmed"));
+*/
+function addNumbers(a,b) {
+  return a + b;
+
+}
+let result = addNumbers(6,0);
+console.log(result);
